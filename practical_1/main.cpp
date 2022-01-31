6d6a433eb8ce888ed7e25ac5164b72b219ad3dd4
Axaerus <40496042@live.napier.ac.uk>
2022-01-31 15:33:36 +0000

    Updated main.cpp
    Updated main.cpp with new test display
    - Now displays magenta circle on top of a green circle in a 300x300 window

--- practical_1/main.cpp
+++ practical_1/main.cpp
@@ -1,9 +1,12 @@
 #include <SFML/Graphics.hpp>
 
 int main(){
-  sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+  sf::RenderWindow window(sf::VideoMode(300, 300), "SFML works!");
   sf::CircleShape shape(100.f);
-  shape.setFillColor(sf::Color::Green);
+  sf::RectangleShape shape2(sf::Vector2(300.f, 300.0f));
+  shape.setFillColor(sf::Color::Magenta);
+  shape2.setFillColor(sf::Color::Green);
+  shape.setPosition(sf::Vector2(50.f, 50.f));
 
   while (window.isOpen()){
       sf::Event event;
@@ -13,6 +16,7 @@ int main(){
       }
     }
     window.clear();
+    window.draw(shape2);
     window.draw(shape);
     window.display();
   }
