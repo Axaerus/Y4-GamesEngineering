5f898afb98e016a8c964a24062131e2cf1d4b6c4
Axaerus <40496042@live.napier.ac.uk>
2022-01-31 17:51:52 +0000

    Pong Rendering and Loading
    Updated Rendering and Loading methods to display paddles and ball

--- practical_2_Pong/main.cpp
+++ practical_2_Pong/main.cpp
@@ -4,7 +4,6 @@ using namespace sf;
 using namespace std;
 
 sf::RenderWindow window(sf::VideoMode(300, 300), "SFML works!");
-sf::Clock clock;
 
 const Keyboard::Key controls[4] = {
     Keyboard::A, //Move player 1 up
@@ -14,32 +13,77 @@ const Keyboard::Key controls[4] = {
 };
 
 const Vector2f paddleSize(25.f, 100.f);
-const float ballRadius = 10.f
+const float ballRadius = 10.f;
+const int gameWidth = 800;
+const int gameHeight = 600;
+const float paddleSpeed = 400.f;
 
+CircleShape ball;
+RectangleShape paddles[2];
 
-void Update(double dt){
-	// Update Everything
+void Load() {
+    //Set size and origin of paddles
+    for (auto& p : paddles) {
+        p.setSize(paddleSize - Vector2f(3, 3));
+        p.setOrigin(paddleSize / 2.f);
+    }
+    //Set size and origin of ball
+    ball.setRadius(ballRadius);
+    ball.setOrigin(Vector2f(gameWidth / 2, gameHeight / 2));
+    paddles[0].setPosition(Vector2(10.f + paddleSize.x / 2.f, gameHeight / 2.f));
+    paddles[1].setPosition(Vector2(10.f + paddleSize.x / 2.f, gameHeight / 2.f));
+    //reset ball position
+    ball.setPosition(Vector2f(gameWidth / 2, gameHeight / 2));
 }
 
-void Render(){
+void Update(RenderWindow &window){
+	//Reset clock, recalculate deltatime
+    static Clock clock;
+    float dt = clock.restart().asSeconds();
+    //check and consume events
+    Event event; 
+    while (window.pollEvent(event)) {
+        if (event.type == Event::Closed) {
+            window.close();
+            return;
+        }
+    }
+
+    //Quite via ESC key
+    if (Keyboard::isKeyPressed(Keyboard::Escape)) {
+        window.close();
+    }
+
+    //handle paddle movement
+    float direction = 0.0f;
+    if (Keyboard::isKeyPressed(controls[0])) {
+        direction--;
+    }
+
+    if (Keyboard::isKeyPressed(controls[1])) {
+        direction++;
+    }
+    paddles[0].move(Vector2(0.f, direction * paddleSpeed * dt));
+}
+
+void Render(RenderWindow &window){
 	// Draw everthing
-    window.display();
+    for (auto& p : paddles) {
+        window.draw(p);
+    }
+    window.draw(ball);
 }
 
 int main(){
-  //initialise and load
+    //initialise and load
+    RenderWindow window(VideoMode(gameWidth, gameHeight), "PONG");
     window.setVerticalSyncEnabled(true);
-    const float dt = clock.restart().asSeconds();
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
-        }
-        /*Update(dt);*/
-        Render();
+    Load();
 
+    while (window.isOpen()) {
+        window.clear();
+        Update(window);
+        Render(window);
     }
-  return 0;
+    return 0;
 }
