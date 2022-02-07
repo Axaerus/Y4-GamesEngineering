bdbb602f13c3cc8d11a239868fc817fe96c21241
Axaerus <40496042@live.napier.ac.uk>
2022-02-07 17:45:36 +0000

    Checkpoint 1 - Displaying Sprite
    Added code to display single sprite as confirmation

--- practical_3_invaders/main.cpp
+++ practical_3_invaders/main.cpp
@@ -1,11 +1,33 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+
+using namespace sf;
+using namespace std;
+
+sf::Texture spritesheet;
+sf::Sprite invader;
+
+sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+sf::RectangleShape shape(sf::Vector2(800.f, 600.0f));
+
 
 void Load() {
     //load method
+    shape.setFillColor(Color::Black);
+
+    if (!spritesheet.loadFromFile("res/img/invaders_sheet.png")) {
+        cerr << "Failed to load spritesheet!" << std::endl;
+    }
+    invader.setTexture(spritesheet);
+    invader.setTextureRect(IntRect(Vector2(0, 0), Vector2(32, 32)));
 }
 
 void Render() {
     //Render method
+    window.clear();
+    window.draw(shape);
+    window.draw(invader);
+    window.display();
 }
 
 void Update() {
@@ -13,12 +35,8 @@ void Update() {
 }
 
 int main(){
-  sf::RenderWindow window(sf::VideoMode(300, 300), "SFML works!");
-  sf::CircleShape shape(100.f);
-  sf::RectangleShape shape2(sf::Vector2(300.f, 300.0f));
-  shape.setFillColor(sf::Color::Magenta);
-  shape2.setFillColor(sf::Color::Green);
-  shape.setPosition(sf::Vector2(50.f, 50.f));
+
+  Load();
 
   while (window.isOpen()){
       sf::Event event;
@@ -27,10 +45,11 @@ int main(){
         window.close();
       }
     }
-    window.clear();
+      Render();
+    /*window.clear();
     window.draw(shape2);
     window.draw(shape);
-    window.display();
+    window.display();*/
   }
   return 0;
 }
