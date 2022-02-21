2ef2374a21100c8f2d7e0f3559a3e48e20a5f2ff
Axaerus <rgater08@gmail.com>
2022-02-21 22:37:05 +0000

    Added Player Class
    Added player class with associated header and cpp
    Added basic controls into Player update method
    Removed .vs file that was present

---
+++ practical_4_TileEngine/player.cpp
@@ -0,0 +1,32 @@
+#include "player.h"
+using namespace sf;
+using namespace std;
+
+void Player::Update(double dt) {
+    //Move in four directions based on 
+    
+    Vector2f direction = Vector2f(0,0);
+    if (Keyboard::isKeyPressed(Keyboard::Left)) {
+        direction.x += -Player::_speed * dt;
+    }
+    if (Keyboard::isKeyPressed(Keyboard::Right)) {
+        direction.x -= Player::_speed * dt;
+    }
+    if (Keyboard::isKeyPressed(Keyboard::Up)) {
+        direction.y -= Player::_speed * dt;
+    }
+    if (Keyboard::isKeyPressed(Keyboard::Down)) {
+        direction.y += Player::_speed * dt;
+    }
+    move(direction);
+    Entity::Update(dt);
+}
+
+Player::Player() : _speed(200.0f), Entity(make_unique<CircleShape>(25.f)) {
+    _shape->setFillColor(Color::Magenta);
+    _shape->setOrigin(Vector2f(25.f, 25.f));
+}
+
+void Player::Render(sf::RenderWindow& window) const {
+    window.draw(*_shape);
+}
